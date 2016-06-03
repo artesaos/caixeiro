753c63d44c260d2707bfbb27ca8591b73c238152
Diego Hernandes <diego@hernandev.com>
2016-06-03 16:50:52 -0300

    few comments

--- src/Exceptions/CaixeiroException.php
+++ src/Exceptions/CaixeiroException.php
@@ -4,6 +4,13 @@ namespace Artesaos\Caixeiro\Exceptions;
 
 use RuntimeException;
 
+/**
+ * CaixeiroException.
+ *
+ * Custom exception for general errors.
+ *
+ * @todo Break exceptions into less generic exception classes.
+ */
 class CaixeiroException extends RuntimeException
 {
     //
